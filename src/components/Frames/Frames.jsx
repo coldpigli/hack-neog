8efabbf1a858c9c827550401cac7eee237d3c825
coldpigli <piyushdas98@gmail.com>
2022-05-08 15:32:17 +0530

    Feat: added onCLicks for orientations

--- src/components/Frames/Frames.jsx
+++ src/components/Frames/Frames.jsx
@@ -3,12 +3,12 @@ import { createRef } from "react";
 import { createFileName, useScreenshot } from "use-react-screenshot";
 import styles from "./Frames.module.css";
 
-const Frames = ({height, width, source, title, icon, landscape, id}) => {
+const Frames = ({ height, width, source, title, icon, landscape, id }) => {
   const ref = createRef(null);
-  const {dispatchDevice} = useDevice();
+  const { dispatchDevice } = useDevice();
   const [image, takeScreenShot] = useScreenshot({
     type: "image/jpeg",
-    quality: 1.0
+    quality: 1.0,
   });
 
   const download = (image, { name = "img", extension = "jpg" } = {}) => {
@@ -20,36 +20,40 @@ const Frames = ({height, width, source, title, icon, landscape, id}) => {
 
   const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
-  const rotateDevice = () => dispatchDevice({type: "ROTATE_DEVICE", payload: id})
+  const rotateDevice = () =>
+    dispatchDevice({ type: "ROTATE_DEVICE", payload: id });
 
   return (
     <div className={styles.emulator}>
-        <div className={styles.frameHeader}>
-          <div className={styles.metaInfo}>
-            <p>{width} X {height}</p>
-          </div>
-            <span className="material-icons md-24">
-             {icon}
-            </span>
-            <div className={styles.screenshot} onClick={downloadScreenshot}>
-            <span className="material-icons md-24">
-              photo_camera
-            </span>
-            </div>
-            <div className={styles.rotate} onClick={rotateDevice
-            }>
-            <span className="material-icons md-24">
-              screen_rotation_alt
-            </span>
-            </div>
-          </div>
-        <div className={styles.frameContainer} style={landscape?{width: height, height: width}:{width: width, height: height}}  ref={ref}>
-            <iframe id={styles.frame} src={source}
-                title={title}> 
-            </iframe>
+      <div className={styles.frameHeader}>
+        <div className={styles.metaInfo}>
+          <p>
+            {width} X {height}
+          </p>
         </div>
+        <span className="material-icons md-24">{icon}</span>
+        <div className={styles.screenshot} onClick={downloadScreenshot}>
+          <span className="material-icons md-24">photo_camera</span>
+        </div>
+        {
+          id!==3?<div className={styles.rotate} onClick={rotateDevice}>
+          <span className="material-icons md-24">screen_rotation_alt</span>
+        </div>:null
+        }
+      </div>
+      <div
+        className={styles.frameContainer}
+        style={
+          landscape
+            ? { width: height, height: width }
+            : { width: width, height: height }
+        }
+        ref={ref}
+      >
+        <iframe id={styles.frame} src={source} title={title}></iframe>
+      </div>
     </div>
-  )
-}   
+  );
+};
 
-export default Frames
+export default Frames;
